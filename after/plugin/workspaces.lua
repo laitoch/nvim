8e62ac15744d62d3dc1e60d792e8bb9729bb5df2
Petr Laitoch <petr.laitoch@customink.com>
2023-09-11 21:03:35 +0200

    add desc

--- after/plugin/workspaces.lua
+++ after/plugin/workspaces.lua
@@ -1,4 +1,9 @@
 require("workspaces").setup({ })
 require("telescope").load_extension("workspaces")
 
-vim.api.nvim_set_keymap("n", "<C-S>", "<cmd>Telescope workspaces<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-S>",
+  "<cmd>Telescope workspaces<CR>",
+  { noremap = true, silent = true, desc = "Open workSpace" }
+)
