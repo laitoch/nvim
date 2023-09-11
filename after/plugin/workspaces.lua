33f373a139f0c990180fcb96eb82ab2826c3c4d8
Petr Laitoch <petr.laitoch@customink.com>
2023-09-11 21:02:33 +0200

    Switch from telescope-project to workspaces
    Allows me to just cd into the correct workspace.
    No need to telescope find_files withing the new cwd.
    New keybinding: <C-s> ... workSpaces.

---
+++ after/plugin/workspaces.lua
@@ -0,0 +1,4 @@
+require("workspaces").setup({ })
+require("telescope").load_extension("workspaces")
+
+vim.api.nvim_set_keymap("n", "<C-S>", "<cmd>Telescope workspaces<CR>", { noremap = true, silent = true })
