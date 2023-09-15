640140cb98bdab74e6a4f1a73500bddff2114942
Petr Laitoch <petr.laitoch@customink.com>
2023-09-15 10:56:05 +0200

    Add keymaps for obsidian

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,1 +1,7 @@
 require("obsidian").setup({})
+
+vim.api.nvim_set_keymap("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ot", "<cmd>ObsidianToday<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>oy", "<cmd>ObsidianYesterday<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>of", "<cmd>ObsidianFollowLink<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { noremap = true, silent = true })
