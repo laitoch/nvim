826c8ce63eff22ec0435f7ed4934a2f9ae7e26df
Petr Laitoch <petr.laitoch@customink.com>
2023-09-15 11:22:40 +0200

    More obsidian mappings

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -20,9 +20,13 @@ require("obsidian").setup({
     end
     return tostring(os.time()) .. "-" .. suffix
   end,
+  finder = "telescope.nvim",
 })
 
 vim.api.nvim_set_keymap("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ot", "<cmd>ObsidianToday<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>oy", "<cmd>ObsidianYesterday<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>of", "<cmd>ObsidianFollowLink<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>on", "<cmd>ObsidianLinkNew<CR>", { noremap = true, silent = true })
