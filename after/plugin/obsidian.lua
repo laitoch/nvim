57c1aca174949f0f52af09d4b2a87e6dc12536fd
Petr Laitoch <petr.laitoch@customink.com>
2023-09-15 16:56:23 +0800

    Setup Obsidian for :ObsidianNew

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,7 +1,28 @@
-require("obsidian").setup({})
+require("obsidian").setup({
+  mappings = {},  -- Needed due to https://github.com/epwalsh/obsidian.nvim/issues/162
+  dir = "~/vault",
+  completion = {
+    new_notes_location = "notes_subdir",
+    prepend_note_id = false,
+  },
+  note_id_func = function(title)
+    -- If title is given, use it to create a note ID.
+    -- Otherwise, use timestamp + random suffix.
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. "-" .. suffix
+  end,
+})
 
 vim.api.nvim_set_keymap("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ot", "<cmd>ObsidianToday<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>oy", "<cmd>ObsidianYesterday<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>of", "<cmd>ObsidianFollowLink<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { noremap = true, silent = true })
