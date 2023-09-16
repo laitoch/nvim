10db1045d5b13e8ae23bdce7ec567f43c56b26b3
Petr Laitoch <petr.laitoch@customink.com>
2023-09-16 21:19:08 +0200

    obsidian follow link keymap upgrade

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap("n", "gf", "<cmd>ObsidianFollowLink<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gl", "<cmd>ObsidianFollowLink<CR>", { noremap = true, silent = true })
 vim.cmd('setlocal textwidth=80')
