fd9216371c20182ef97c965f6c95fc938740edf1
Petr Laitoch <petr.laitoch@customink.com>
2023-11-15 01:54:23 +0100

    put neogit back via author-column-recent-commits branch

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,10 @@
+return {
+  "treatybreaker/neogit",
+  branch = "feat/author-column-recent-commits",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim",        -- optional
+  },
+  config = true
+}
