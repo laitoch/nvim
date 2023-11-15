1ba92a89242290dae501f957f3662fbef124739b
Petr Laitoch <petr.laitoch@customink.com>
2023-11-15 01:55:36 +0100

    StupidWIP: vim-ai write commit message via chatgpt

--- after/plugin/vim-ai.lua
+++ after/plugin/vim-ai.lua
@@ -4,3 +4,7 @@ vim.g.vim_ai_chat = {
       temperature = 0.2,
   }
 }
+
+
+-- vim.cmd('vnew | r ! git diff --staged')
+-- %AIE Write git commit message ready to copy-paste in GIT-EDITMSG. Output format is a title (one), a blank line (one) and then bullet points
