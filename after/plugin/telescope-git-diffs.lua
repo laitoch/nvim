a1220b0b75cfebe244909b967ff3afd724354afa
Petr Laitoch <petr.laitoch@customink.com>
2023-11-15 13:29:17 +0100

    WIP tried to add auther and date info into telescope-git-diffs; Not done

--- after/plugin/telescope-git-diffs.lua
+++ after/plugin/telescope-git-diffs.lua
@@ -1,3 +1,20 @@
+-- require('telescope').setup {
+--     extensions = {
+--         git_diffs = {
+--             git_command = {
+--               "git",
+--               "log",
+--               "--decorate",
+--               "--all",
+--               "--date=relative",
+--               -- "--oneline",
+--               '--pretty=format:\"%h%x09%<(7,trunc)%an%x08%x08%x09%ad%x09%s\"',
+--               ".",
+--             }
+--         }
+--     }
+-- }
+
 require('telescope').load_extension('git_diffs')
 
 vim.api.nvim_set_keymap(
