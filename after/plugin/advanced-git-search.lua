f25fd258639d113207dc9ec220e296d2312fe7d7
Petr Laitoch <petr.laitoch@customink.com>
2023-11-15 23:34:39 +0100

    Add advanced git search

---
+++ after/plugin/advanced-git-search.lua
@@ -0,0 +1,43 @@
+vim.keymap.set(
+  'n',
+  '<leader>f1',
+  function()
+    vim.cmd('Telescope advanced_git_search diff_commit_file')
+  end,
+  { desc = 'Open vifm in floaterm' }
+)
+
+
+vim.keymap.set(
+  'n',
+  '<leader>f2',
+  function()
+    vim.cmd('Telescope advanced_git_search search_log_content')
+  end,
+  { desc = 'Open vifm in floaterm' }
+)
+
+
+vim.keymap.set(
+  'n',
+  '<leader>f3',
+  function()
+    vim.cmd('Telescope advanced_git_search search_log_content_file')
+  end,
+  { desc = 'Open vifm in floaterm' }
+)
+
+
+-- :'<,'>AdvancedGitSearch diff_commit_line
+
+
+vim.keymap.set(
+  'n',
+  '<leader>f4',
+  function()
+    vim.cmd('Telescope advanced_git_search changed_on_branch')
+  end,
+  { desc = 'Open vifm in floaterm' }
+)
+
+
