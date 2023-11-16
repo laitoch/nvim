6c6eadcba2b3f7f0b02b5caeb0a126b01f443eb1
Petr Laitoch <petr.laitoch@customink.com>
2023-11-17 00:34:34 +0100

    :lipstick:

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap("n", "<Leader>db", "<Cmd>DBUIToggle<Cr>", { noremap = true, silent = true, desc = "Toggle UI" })
-vim.api.nvim_set_keymap("n", "<Leader>df", "<Cmd>DBUIFindBuffer<Cr>", { noremap = true, silent = true, desc = "Find buffer" })
-vim.api.nvim_set_keymap("n", "<Leader>dr", "<Cmd>DBUIRenameBuffer<Cr>", { noremap = true, silent = true, desc = "Rename buffer" })
-vim.api.nvim_set_keymap("n", "<Leader>dq", "<Cmd>DBUILastQueryInfo<Cr>", { noremap = true, silent = true, desc = "Last query info" })
+vim.api.nvim_set_keymap("n", "<leader>db", "<Cmd>DBUIToggle<Cr>", { noremap = true, silent = true, desc = "Toggle UI" })
+vim.api.nvim_set_keymap("n", "<leader>df", "<Cmd>DBUIFindBuffer<Cr>", { noremap = true, silent = true, desc = "Find buffer" })
+vim.api.nvim_set_keymap("n", "<leader>dr", "<Cmd>DBUIRenameBuffer<Cr>", { noremap = true, silent = true, desc = "Rename buffer" })
+vim.api.nvim_set_keymap("n", "<leader>dq", "<Cmd>DBUILastQueryInfo<Cr>", { noremap = true, silent = true, desc = "Last query info" })
