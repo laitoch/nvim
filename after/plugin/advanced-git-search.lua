0c5eebaa5a9aa53d3b03abd954f639dbd284a9cb
Petr Laitoch <petr.laitoch@customink.com>
2023-11-16 13:34:17 +0100

    Nicer maps for Telescope <leader>f and <leader>g

--- after/plugin/advanced-git-search.lua
+++ after/plugin/advanced-git-search.lua
@@ -1,45 +1,44 @@
-
-
 vim.keymap.set(
   'n',
-  '<leader>f2',
+  '<leader>gr',
   function()
     vim.cmd('Telescope advanced_git_search search_log_content')
   end,
-  { desc = 'Open vifm in floaterm' }
+  { desc = 'Git Repository (code filter) (Telescope)' }
 )
-
 
 vim.keymap.set(
   'n',
-  '<leader>f3',
+  '<leader>gf',
   function()
     vim.cmd('Telescope advanced_git_search search_log_content_file')
   end,
-  { desc = 'Open vifm in floaterm' }
+  { desc = 'Git File (code filter) (Telescope)' }
 )
-
--- Blah blah
-
-
--- :'<,'>AdvancedGitSearch diff_commit_line
-
 
 vim.keymap.set(
   'n',
-  '<leader>f4',
-  function()
-    vim.cmd('Telescope advanced_git_search changed_on_branch')
-  end,
-  { desc = 'Open vifm in floaterm' }
-)
-
-
-vim.keymap.set(
-  'n',
-  '<leader>f1',
+  '<leader>gF',
   function()
     vim.cmd('Telescope advanced_git_search diff_commit_file')
   end,
-  { desc = 'Open vifm in floaterm' }
+  { desc = 'Git File (commit message filter) (Telescope)' }
+)
+
+vim.keymap.set(
+  'n',
+  '<leader>gb',
+  function()
+    vim.cmd('Telescope advanced_git_search changed_on_branch')
+  end,
+  { desc = 'Git Branch compare like a github pr (Telescope)' }
+)
+
+vim.keymap.set(
+  'v',
+  '<C-B>',
+  function()
+    vim.cmd("'<,'>AdvancedGitSearch diff_commit_line")
+  end,
+  { desc = 'Blame lines (Telescope)' }
 )
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,33 +1,47 @@
 local builtin = require('telescope.builtin')
 
--- TODO: The line below shold be a which-key group, not a comment.
---
--- <leader>f = find
-
-vim.keymap.set('n', '<leader>fF', function()
-  builtin.find_files{
-    cwd = vim.fn.expand('%:p:h'),
-    no_ignore = true,
-  }
-end)
-vim.keymap.set('n', '<leader>fg', function()
-  builtin.grep_string({
-    cwd = vim.fn.expand('%:p:h'),
-    search = vim.fn.input("Grep > "),
-  })
-end)
 vim.keymap.set(
   'n',
   '<leader>ff',
-  function() builtin.git_files({ show_untracked = true }) end
+  function() builtin.git_files({ show_untracked = true }) end,
+  { desc = 'Find git File' }
 )
+
+vim.keymap.set(
+  'n',
+  '<leader>fF',
+  function()
+    builtin.find_files{
+      cwd = vim.fn.expand('%:p:h'),
+      no_ignore = true,
+    }
+  end,
+  { desc = 'Find local Files in current directory' }
+)
+
 vim.keymap.set(
   'n',
   '<leader>fg',
-  function() require('git_grep').live_grep() end  -- TODO: needs show_untracked also?
+  function() require('git_grep').live_grep() end,
+  { desc = 'Find Git Grep' }
 )
-vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>")
-vim.keymap.set('n', '<leader>fm', ":Telescope marks<CR>")
-vim.keymap.set('n', '<leader>fl', ":Telescope current_buffer_fuzzy_find<CR>")
-vim.keymap.set('n', '<leader>fr', ":Telescope oldfiles<CR>")
-vim.keymap.set('n', '<leader>fc', ":Telescope command_history<CR>")
+
+vim.keymap.set(
+  'n',
+  '<leader>fG',
+  function()
+    builtin.grep_string({
+      cwd = vim.fn.expand('%:p:h'),
+      search = vim.fn.input("Grep > "),
+    })
+  end,
+  { desc = 'Find local Grep in current directory' }
+)
+
+-- vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>")
+vim.keymap.set('n', '<leader>fm', ":Telescope marks<CR>", { desc = 'Find Marks' })
+vim.keymap.set('n', '<leader>fl', ":Telescope current_buffer_fuzzy_find<CR>", { desc = 'Find Line in current buffer' })
+vim.keymap.set('n', '<leader>fr', ":Telescope oldfiles<CR>", { desc = 'Find Recently opened files' })
+vim.keymap.set('n', '<leader>fc', ":Telescope command_history<CR>", { desc = 'Find Command history' })
+
+vim.keymap.set('n', '<leader>gc', ":Telescope git_commits<CR>", { desc = 'Git Commits (commit message filter) (Telescope)' })
