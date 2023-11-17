616ee96016633b105964c9038a0b6240667cba46
Petr Laitoch <petr.laitoch@customink.com>
2023-11-17 01:27:42 +0100

    Move lsp keymaps from whichkey

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -61,3 +61,27 @@ require('lspconfig').lua_ls.setup({
     }
   }
 })
+
+-- LSP Keymaps
+local wk = require("which-key")
+wk.register({
+  ["<leader>ca"] = { vim.lsp.buf.code_action, "[C]ode [A]ction (LSP)" },
+  ["<leader>rn"] = { vim.lsp.buf.rename, "[R]e[n]ame (LSP)" },
+  ["<leader>gd"] = { vim.lsp.buf.definition, "[G]o to [D]efinition (LSP)" },
+  ["<leader>gr"] = { "<cmd>Telescope lsp_references<cr>", "[G]o to [R]eferences (LSP + Telescope)" },
+  ["K"] = { vim.lsp.buf.hover, "LSP Hover" },
+
+  ["<leader>fa"] = {
+    function()
+      vim.lsp.buf.code_action({filter=function(arg) return arg.title == "Ruff: Fix All" end, apply=true})
+    end,
+    "[F]ix [A]ll (Ruff)",
+  },
+  ["<leader>oi"] = {
+    function()
+      vim.lsp.buf.code_action({filter=function(arg) return arg.title == "Ruff: Organize Imports" end, apply=true})
+    end,
+    "[O]rganize [I]mports (Ruff)",
+  },
+})
+
