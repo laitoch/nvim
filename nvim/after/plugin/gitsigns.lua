a79275c42544dd7c9ae7a3006d662a872442767b
Petr Laitoch <petr.laitoch@customink.com>
2024-06-10 13:31:05 +0200

    Add descriptions to <leader>h*

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -25,18 +25,18 @@ require('gitsigns').setup {
       end, {expr=true})
 
       -- Actions
-      map('n', '<leader>hs', gs.stage_hunk)
-      map('n', '<leader>hr', gs.reset_hunk)
-      map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-      map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-      map('n', '<leader>hS', gs.stage_buffer)
-      map('n', '<leader>hu', gs.undo_stage_hunk)
-      map('n', '<leader>hR', gs.reset_buffer)
-      map('n', '<leader>hp', gs.preview_hunk)
-      map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-      map('n', '<leader>hb', gs.toggle_current_line_blame)
-      map('n', '<leader>hd', gs.diffthis)
-      map('n', '<leader>hD', function() gs.diffthis('~') end)
+      map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage hunk' })
+      map('n', '<leader>hr', gs.reset_hunk, { decs = 'Reset hunk' })
+      map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage hunk' })
+      map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Reset hunk' })
+      map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage buffer' })
+      map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo stage hunk' })
+      map('n', '<leader>hR', gs.reset_buffer, {desc = 'Reset buffer' })
+      map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview hunk' })
+      map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Blame line'})
+      map('n', '<leader>hB', gs.toggle_current_line_blame, { desc = 'Toggle current line blame' })
+      map('n', '<leader>hd', gs.diffthis, { desc = 'Diff this' })
+      map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff this ~' })
       -- map('n', ',td', gs.toggle_deleted) -- TODO whats this?
 
       -- Text object
